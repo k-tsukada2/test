9789bda672ab772ea13a574e52c5c8c82aa5a02a
塚田 浩太郎 <tsukada@dhcp14.galileo.lan>
2015-03-19 16:55:39 +0900

    appフォルダ

---
+++ app/hello/hello.go
@@ -0,0 +1,14 @@
+package hello
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, world! สวัสดี เรย์มุ วันนี้ก็น่ารักจังเลย!")
+}
