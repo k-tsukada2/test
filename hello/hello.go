a91d5903db876f72c0c7f8b76eb08e1856e2b1df
塚田 浩太郎 <tsukada@dhcp14.galileo.lan>
2015-03-19 17:20:48 +0900

    test

--- hello/hello.go
+++ hello/hello.go
@@ -10,5 +10,5 @@ func init() {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!\nGo script is written in utf8.\nสวัสดี เรย์มุ วันนี้ก็น่ารักจังเลย!")
+	fmt.Fprint(w, "Hello, world!<br />Go script is written in utf8.<br />สวัสดี เรย์มุ วันนี้ก็น่ารักจังเลย!")
 }
